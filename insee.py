69122a554830caa283dd7f5c564aea0db6792d9a
Maxime Saddok <saddok_m@etna-alternance.net>
2014-04-24 01:34:19 +0200

    test merging dataframes : insee+accidents

--- insee.py
+++ insee.py
@@ -5,10 +5,19 @@ from numpy import random
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# data files
+insee_data = "data/insee_postal.csv"
+accid_data = "data/accidents.csv"
 
-filename = "data/insee_postal.csv"
-df = read_csv(filename, sep=";")
+# readings CSVs and parsing with separator 
+insee_df = read_csv(insee_data, sep=";")
+accid_df = read_csv(accid_data, sep=",")
 
-df2 = DataFrame(data=df, columns=['insee_com','postal_code','nom_comm','nom_dept','nom_region', 'geo_shape', 'code_comm', 'code_dept'])
+# selecting columns
+insee_df = DataFrame(data=insee_df, columns=['code_comm', 'insee_com', 'nom_comm', 'postal_code', 'nom_dept', 'nom_region', 'geo_shape', 'code_dept'])
+accid_df = DataFrame(data=accid_df, columns=['numac', 'lum', 'agg', 'int', 'atm', 'col', 'adr', 'dep', 'code_comm', 'gps', 'lat', 'long', 'catr', 'voie', 'v1', 'v2', 'infra', 'situ', 'circ', 'nbv', 'vosp', 'prof', 'plan', 'ttue', 'tbg', 'tbl', 'tindm', 'numero', 'libellevoie', 'grav'])
 
-print(df2.head)
+
+merged = DataFrame.merge(accid_df, insee_df, how='outer', on='code_comm', copy=False)
+
+print(merged.info)
