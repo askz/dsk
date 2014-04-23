de18bf2c6d5f429cb42d1bbbbb0140c8d94d292c
ASKz <askzz@me.com>
2014-04-23 22:53:07 +0000

    added: check if data/ dir exists

--- bigdata.py
+++ bigdata.py
@@ -5,14 +5,19 @@ import matplotlib as mpl
 from descartes import PolygonPatch
 import json as simplejson
 import numpy as np
+import os
 
-data = pandas.read_csv("data/accidents.csv",na_values='9999')
-# data = pandas.read_csv("vehicules.csv",dtype=object)
+if os.path.isdir("data"):
+  data = pandas.read_csv("data/accidents.csv",na_values='9999')
+else:
+  print("Please run 'make download' to grab data required by this tool!\n")
+#data = pandas.read_csv("vehicules.csv",dtype=object)
 com = data['com']
 
 print(com)
 
-counts = com.value_counts(sort=False)
+counts = com.value_counts(sort=True)
+
 print(counts)
 
 
