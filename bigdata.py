b3fb4b4e75a6348626b629960116a4afa02b7020
ASKz <askzz@me.com>
2014-04-23 17:07:13 +0000

    json bug removed

--- bigdata.py
+++ bigdata.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python3
 import pandas
 from matplotlib import pyplot as plt
 import matplotlib as mpl
 from descartes import PolygonPatch
-import simplejson
+import json as simplejson
 import numpy as np
 
-data = pandas.read_csv("accidents.csv",na_values='9999')
+data = pandas.read_csv("pub/accidents.csv",na_values='9999')
 # data = pandas.read_csv("vehicules.csv",dtype=object)
 com = data['com']
 
