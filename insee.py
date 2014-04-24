a58cab9f7a6cf27f81b21f4a7ce7266a498f3d33
ASKz <askzz@me.com>
2014-04-24 09:21:44 +0000

    concat

--- insee.py
+++ insee.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from pandas import DataFrame, read_csv
+from pandas import DataFrame, merge, read_csv
 from numpy import random
 
 import matplotlib.pyplot as plt
@@ -16,15 +16,22 @@ accid_df = read_csv(accid_data, sep=",")
 # selecting columns
 #insee_df = DataFrame(data=insee_df,  columns=['code_comm', 'insee_com', 'nom_comm', 'postal_code', 'nom_dept', 'nom_region', 'geo_shape', 'code_dept'])
 insee_df = DataFrame(data=insee_df,  columns=['code_comm', 'insee_com', 'nom_comm'])
+
 insee_df.set_index('code_comm')
 
 #accid_df = DataFrame(data=accid_df,  columns=['numac', 'lum', 'agg', 'int', 'atm', 'col', 'adr', 'dep', 'code_comm', 'gps', 'lat', 'long', 'catr', 'voie', 'v1', 'v2', 'infra', 'situ', 'circ', 'nbv', 'vosp', 'prof', 'plan', 'ttue', 'tbg', 'tbl', 'tindm', 'numero', 'libellevoie', 'grav'])
 accid_df = DataFrame(data=accid_df,  columns=['numac', 'agg', 'atm', 'col', 'adr', 'dep', 'code_comm', 'lat', 'long'])
 
+code_comm = accid_df['code_comm']
+counts = code_comm.value_counts(sort=True)
 
+print(counts)
 
+counts.astype(int)
 
+counts = DataFrame(data=counts)
 # merging DataFrames
-merged = pd.merge(accid_df, insee_df, on='code_comm', how='outer', sort=False)
-
-print(merged.dropna)
+#merged = merge(insee_df, counts,  how='outer', sort=True).fillna(0)
+concatened = pd.concat((insee_df, counts), axis=1)
+print(concatened.head)
+print(concatened.tail)
