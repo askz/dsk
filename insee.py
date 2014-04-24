aa29453340c9d7364d71d50ae5ebe91bf2c71bbc
Maxime Saddok <saddok_m@etna-alternance.net>
2014-04-24 17:11:47 +0200

    insee.py: generating ok.csv wich contains correspondance with cities and number of accidents.

--- insee.py
+++ insee.py
@@ -16,26 +16,27 @@ accid_df = read_csv(accid_data, sep=",")
 # selecting columns
 #insee_df = DataFrame(data=insee_df,  columns=['code_comm', 'insee_com', 'nom_comm', 'postal_code', 'nom_dept', 'nom_region', 'geo_shape', 'code_dept'])
 insee_df = DataFrame(data=insee_df,  columns=['code_comm', 'insee_com', 'nom_comm'])
-
 insee_df.set_index('code_comm')
-print(insee_df.describe)
-#accid_df = DataFrame(data=accid_df,  columns=['numac', 'lum', 'agg', 'int', 'atm', 'col', 'adr', 'dep', 'code_comm', 'gps', 'lat', 'long', 'catr', 'voie', 'v1', 'v2', 'infra', 'situ', 'circ', 'nbv', 'vosp', 'prof', 'plan', 'ttue', 'tbg', 'tbl', 'tindm', 'numero', 'libellevoie', 'grav'])
-accid_df = DataFrame(data=accid_df,  columns=['numac', 'agg', 'atm', 'col', 'adr', 'dep', 'code_comm', 'lat', 'long'])
-print(accid_df.describe)
+
+accid_df = DataFrame(data=accid_df,  columns=['numac', 'lum', 'agg', 'int', 'atm', 'col', 'adr', 'dep', 'code_comm', 'gps', 'lat', 'long', 'catr', 'voie', 'v1', 'v2', 'infra', 'situ', 'circ', 'nbv', 'vosp', 'prof', 'plan', 'ttue', 'tbg', 'tbl', 'tindm', 'numero', 'libellevoie', 'grav'])
+#accid_df = DataFrame(data=accid_df,  columns=['numac', 'agg', 'atm', 'col', 'adr', 'dep', 'code_comm', 'lat', 'long'])
+
 
 code_comm = accid_df['code_comm']
-counts = code_comm.value_counts(sort=True)
+ttue = accid_df['ttue']
 
+ttue_count = ttue.value_counts(sort=True)
+comm_count = code_comm.value_counts(sort=True)
 
+comm_count = comm_count.astype(int)
+comm_count = DataFrame(data=comm_count)
 
-counts = counts.astype(int)
+comm_count.columns = ['nbr_acc']
+insee_df.set_index('code_comm')
+# concat DataFrames
+concatened = pd.concat((insee_df, comm_count), axis=1)
 
-counts = DataFrame(data=counts)
-# merging DataFrames
+concatened.fillna(0)
 
-#merged = merge(insee_df, counts,  how='outer', sort=True).fillna(0)
-concatened = pd.concat((insee_df, counts), axis=1)
-
-
-concatened.to_csv('concat.csv')
+concatened.to_csv('ok.csv')
 print(concatened.describe())
