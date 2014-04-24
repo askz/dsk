eebf64e139d1bc75876df6e36816e7ba2be192cf
Maxime Saddok <saddok_m@etna-alternance.net>
2014-04-24 18:34:37 +0200

    insee.py: added geoshape column to final csv.

--- insee.py
+++ insee.py
@@ -15,16 +15,16 @@ accid_df = read_csv(accid_data, sep=",")
 
 # selecting columns
 #insee_df = DataFrame(data=insee_df,  columns=['code_comm', 'insee_com', 'nom_comm', 'postal_code', 'nom_dept', 'nom_region', 'geo_shape', 'code_dept'])
-insee_df = DataFrame(data=insee_df,  columns=['code_comm', 'nom_comm'])
+insee_df = DataFrame(data=insee_df,  columns=['code_comm', 'nom_comm', 'geo_shape'])
 insee_df.set_index('code_comm')
 
-accid_df = DataFrame(data=accid_df,  columns=['numac', 'lum', 'agg', 'int', 'atm', 'col', 'adr', 'dep', 'code_comm', 'gps', 'lat', 'long', 'catr', 'voie', 'v1', 'v2', 'infra', 'situ', 'circ', 'nbv', 'vosp', 'prof', 'plan', 'ttue', 'tbg', 'tbl', 'tindm', 'numero', 'libellevoie', 'grav'])
-#accid_df = DataFrame(data=accid_df,  columns=['numac', 'agg', 'atm', 'col', 'adr', 'dep', 'code_comm', 'lat', 'long'])
+#accid_df = DataFrame(data=accid_df,  columns=['numac', 'lum', 'agg', 'int', 'atm', 'col', 'adr', 'dep', 'code_comm', 'gps', 'lat', 'long', 'catr', 'voie', 'v1', 'v2', 'infra', 'situ', 'circ', 'nbv', 'vosp', 'prof', 'plan', 'ttue', 'tbg', 'tbl', 'tindm', 'numero', 'libellevoie', 'grav'])
+accid_df = DataFrame(data=accid_df,  columns=['agg', 'atm', 'col', 'adr', 'dep', 'code_comm', 'lat', 'long'])
 
 code_comm = accid_df['code_comm']
-ttue = accid_df['ttue']
+#ttue = accid_df['ttue']
 
-ttue_count = ttue.value_counts(sort=True)
+#ttue_count = ttue.value_counts(sort=True)
 comm_count = code_comm.value_counts(sort=True)
 
 comm_count = comm_count.astype(int)
@@ -36,7 +36,7 @@ insee_df.set_index('code_comm')
 # concat DataFrames
 concatened = pd.concat((insee_df, comm_count), axis=1)
 
-concatened.fillna(0)
+concatened = concatened.fillna(0)
 
 concatened.to_csv('data/accidents_localised.csv')
 print(concatened.describe())
