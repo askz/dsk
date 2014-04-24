48198a1a6dc961f6dac5a1a45b17e5dc04c6ac00
ASKz <askzz@me.com>
2014-04-24 08:05:48 +0000

    working on DataFrames merging

--- insee.py
+++ insee.py
@@ -14,10 +14,17 @@ insee_df = read_csv(insee_data, sep=";")
 accid_df = read_csv(accid_data, sep=",")
 
 # selecting columns
-insee_df = DataFrame(data=insee_df, columns=['code_comm', 'insee_com', 'nom_comm', 'postal_code', 'nom_dept', 'nom_region', 'geo_shape', 'code_dept'])
-accid_df = DataFrame(data=accid_df, columns=['numac', 'lum', 'agg', 'int', 'atm', 'col', 'adr', 'dep', 'code_comm', 'gps', 'lat', 'long', 'catr', 'voie', 'v1', 'v2', 'infra', 'situ', 'circ', 'nbv', 'vosp', 'prof', 'plan', 'ttue', 'tbg', 'tbl', 'tindm', 'numero', 'libellevoie', 'grav'])
+#insee_df = DataFrame(data=insee_df,  columns=['code_comm', 'insee_com', 'nom_comm', 'postal_code', 'nom_dept', 'nom_region', 'geo_shape', 'code_dept'])
+insee_df = DataFrame(data=insee_df,  columns=['code_comm', 'insee_com', 'nom_comm'])
+insee_df.set_index('code_comm')
+
+#accid_df = DataFrame(data=accid_df,  columns=['numac', 'lum', 'agg', 'int', 'atm', 'col', 'adr', 'dep', 'code_comm', 'gps', 'lat', 'long', 'catr', 'voie', 'v1', 'v2', 'infra', 'situ', 'circ', 'nbv', 'vosp', 'prof', 'plan', 'ttue', 'tbg', 'tbl', 'tindm', 'numero', 'libellevoie', 'grav'])
+accid_df = DataFrame(data=accid_df,  columns=['numac', 'agg', 'atm', 'col', 'adr', 'dep', 'code_comm', 'lat', 'long'])
 
 
-merged = DataFrame.merge(accid_df, insee_df, how='outer', on='code_comm', copy=False)
 
-print(merged.info)
+
+# merging DataFrames
+merged = pd.merge(accid_df, insee_df, on='code_comm', how='outer', sort=False)
+
+print(merged.dropna)
