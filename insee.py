d33d16041d6cc65737f62a0ecda0f0fbe0505778
Maxime Saddok <saddok_m@etna-alternance.net>
2014-04-24 10:22:52 +0200

    insee

--- insee.py
+++ insee.py
@@ -16,6 +16,7 @@ accid_df = read_csv(accid_data, sep=",")
 # selecting columns
 #insee_df = DataFrame(data=insee_df,  columns=['code_comm', 'insee_com', 'nom_comm', 'postal_code', 'nom_dept', 'nom_region', 'geo_shape', 'code_dept'])
 insee_df = DataFrame(data=insee_df,  columns=['code_comm', 'insee_com', 'nom_comm'])
+
 insee_df.set_index('code_comm')
 
 #accid_df = DataFrame(data=accid_df,  columns=['numac', 'lum', 'agg', 'int', 'atm', 'col', 'adr', 'dep', 'code_comm', 'gps', 'lat', 'long', 'catr', 'voie', 'v1', 'v2', 'infra', 'situ', 'circ', 'nbv', 'vosp', 'prof', 'plan', 'ttue', 'tbg', 'tbl', 'tindm', 'numero', 'libellevoie', 'grav'])
@@ -25,6 +26,7 @@ accid_df = DataFrame(data=accid_df,  columns=['numac', 'agg', 'atm', 'col', 'adr
 
 
 # merging DataFrames
-merged = pd.merge(accid_df, insee_df, on='code_comm', how='outer', sort=False)
+merged = pd.merge(accid_df, insee_df, on='code_comm', how='outer', sort=True)
 
-print(merged.dropna)
+merged.to_csv('lol.csv')
+
