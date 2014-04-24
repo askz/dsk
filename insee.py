a8c7ab7609b8454a5f96590f6900008a2a4726b2
Maxime Saddok <saddok_m@etna-alternance.net>
2014-04-24 14:14:56 +0200

    insee.py: generates concat.csv with nom_com, insee_com, code_comm, last row is number of accidents, THIS ISNT THE REAL COUNT, be aware :)

--- insee.py
+++ insee.py
@@ -18,21 +18,24 @@ accid_df = read_csv(accid_data, sep=",")
 insee_df = DataFrame(data=insee_df,  columns=['code_comm', 'insee_com', 'nom_comm'])
 
 insee_df.set_index('code_comm')
-
+print(insee_df.describe)
 #accid_df = DataFrame(data=accid_df,  columns=['numac', 'lum', 'agg', 'int', 'atm', 'col', 'adr', 'dep', 'code_comm', 'gps', 'lat', 'long', 'catr', 'voie', 'v1', 'v2', 'infra', 'situ', 'circ', 'nbv', 'vosp', 'prof', 'plan', 'ttue', 'tbg', 'tbl', 'tindm', 'numero', 'libellevoie', 'grav'])
 accid_df = DataFrame(data=accid_df,  columns=['numac', 'agg', 'atm', 'col', 'adr', 'dep', 'code_comm', 'lat', 'long'])
+print(accid_df.describe)
 
 code_comm = accid_df['code_comm']
 counts = code_comm.value_counts(sort=True)
 
-print(counts)
 
-counts.astype(int)
+
+counts = counts.astype(int)
 
 counts = DataFrame(data=counts)
 # merging DataFrames
 
 #merged = merge(insee_df, counts,  how='outer', sort=True).fillna(0)
 concatened = pd.concat((insee_df, counts), axis=1)
-print(concatened.head)
-print(concatened.tail)
+
+
+concatened.to_csv('concat.csv')
+print(concatened.describe)
