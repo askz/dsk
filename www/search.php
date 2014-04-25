8c10e128765d25e12fa73349f7c2ddaccbd5c2ab
Wildine Anthony <wiz@wiz>
2014-04-26 00:35:52 +0200

    web2

---
+++ www/search.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Driving Safety Keeper</title>
+		<link rel="stylesheet" href="search.css" />
+		<meta charset="utf-8" />
+	</head>
+
+	<body>
+		<div id="global">
+			<?php include("header.php");?>
+			<div id="contenu">
+				
+					<section>
+					 	<form>
+					   		<label for="search">Rechercher :</label>
+					   		<input id="search" type="search" placeholder="Recherche"/>
+					   		<br />
+					   		<input type="checkbox" name="choix1" value="nb_acc">Nombre d'accidents</input>
+					   		<input type="checkbox" name="choix2" value="">test</input>
+					   		<input type="checkbox" name="choix3" value="">test</input>
+					   </form>
+					</section>					
+			</div>
+		</div>
+		<?php include("footer.php");?>
+	</body>
+</html>
